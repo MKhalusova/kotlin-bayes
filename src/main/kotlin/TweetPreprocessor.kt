2e2bb24eb255b5d803cbf5b55d0dc941dd4ee794
MKhalusova <kafooster@gmail.com>
2020-09-15 09:10:47 -0400

    minor polishing

--- src/main/kotlin/TweetPreprocessor.kt
+++ src/main/kotlin/TweetPreprocessor.kt
@@ -2,25 +2,25 @@
 
 class TweetPreprocessor {
     // remove stock market tickers like $GE
-    private fun String.removeTickers() = replace(regex = Regex("\\\$\\w*"), replacement = "")
+    private fun String.removeTickers() = replace(Regex("\\\$\\w*"), "")
 
     // remove old style retweet text "RT"
-    private fun String.removeRTs() = replace(regex = Regex("^RT[\\s]+"), replacement = "")
+    private fun String.removeRTs() = replace(Regex("^RT[\\s]+"), "")
 
     // remove URLs
-    private fun String.removeURLs() = replace(regex = Regex("https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"), "")
+    private fun String.removeURLs() = replace(Regex("https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"), "")
 
     // remove hashtags
     private fun String.removeHashtags() = replace("#", "")
 
     // remove mentions
-    private fun String.removeMentions() = replace(regex = Regex("[@#][\\w_-]+"), replacement ="")
+    private fun String.removeMentions() = replace(Regex("[@#][\\w_-]+"), "")
 
     // remove XML character encodings like &amp;
-    private fun String.removeXMLEncodings() = replace(regex = Regex("&[a-z]*;")," ")
+    private fun String.removeXMLEncodings() = replace(Regex("&[a-z]*;")," ")
 
     // remove extra spaces
-    private fun String.removeExtraSpaces() = replace(regex = Regex("\\s+")," ")
+    private fun String.removeExtraSpaces() = replace(Regex("\\s+")," ")
 
     private fun String.cleanTweet(): String {
         return this.removeTickers().removeRTs().removeURLs().removeHashtags().removeMentions().removeXMLEncodings().removeExtraSpaces()
